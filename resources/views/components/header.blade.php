aee374e1bdd9d47568f352d25405d6e2bac064ff
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-11-17 16:04:15 +0900

    完成

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -14,9 +14,9 @@
                                 <option value="">全て</option>
                                 @foreach ($categories as $category)
                                 <option value="primary:{{$category->id}}" class="font-weight-bold" {{ $defaults['category'] == "primary:" . $category->id ? 'selected' : ''}}>{{$category->name}}</option>
-                                    @foreach ($category->secondaryCategories as $secondary)
-                                    <option value="secondary:{{$secondary->id}}" {{ $defaults['category'] == "secondary:" . $secondary->id ? 'selected' : ''}}>　{{$secondary->name}}</option>
-                                    @endforeach
+                                @foreach ($category->secondaryCategories as $secondary)
+                                <option value="secondary:{{$secondary->id}}" {{ $defaults['category'] == "secondary:" . $secondary->id ? 'selected' : ''}}>　{{$secondary->name}}</option>
+                                @endforeach
                                 @endforeach
                             </select>
                         </div>
@@ -29,7 +29,7 @@
                     </div>
                 </form>
                 @guest
-                {{-- 非ログイン --}}
+
                 <li class="nav-item">
                     <a class="btn btn-secondary ml-3" href="{{ route('register') }}" role="button">会員登録</a>
                 </li>
@@ -37,9 +37,9 @@
                     <a class="btn btn-outline-info ml-2" href="{{ route('login') }}" role="button">ログイン</a>
                 </li>
                 @else
-                {{-- ログイン済み --}}
+
                 <li class="nav-item dropdown ml-2">
-                    {{-- ログイン情報 --}}
+
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         @if (!empty($user->avatar_file_name))
                         <img src="/storage/avatars/{{$user->avatar_file_name}}" class="rounded-circle" style="object-fit: cover; width: 35px; height: 35px;">
@@ -48,8 +48,24 @@
                         @endif
                         {{ $user->name }} <span class="caret"></span>
                     </a>
-                    {{-- ドロップダウンメニュー --}}
+
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <div class="dropdown-item-text">
+                            <div class="row no-gutters">
+                                <div class="col">売上金</div>
+                                <div class="col-auto">
+                                    <i class="fas fa-yen-sign"></i>
+                                    <span class="ml-1">{{number_format($user->sales)}}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="dropdown-item-text">
+                            <div class="row no-gutters">
+                                <div class="col">出品数</div>
+                                <div class="col-auto">{{number_format($user->soldItems->count())}} 個</div>
+                            </div>
+                        </div>
+                        <div class="dropdown-divider"></div>
                         <a href="{{ route('sell') }}" class="dropdown-item">
                             <i class="fas fa-camera text-left" style="width: 30px"></i>商品を出品する
                         </a>
