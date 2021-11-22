eab47e82a52b74e70e07a313bb026a6b5a97e912
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-11-22 18:27:58 +0900

    修正

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -42,7 +42,7 @@ class ItemsController extends Controller
             });
         }
 
-        $items = $query->orderByRaw("FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')")
+        $items = $query->orderBy("FIELD(state, '" . Item::STATE_SELLING . "', '" . Item::STATE_BOUGHT . "')")
             ->orderBy('id', 'DESC')
             ->paginate(40);
         // orderByRawメソッドを使って、出品中の商品を先に、購入済みの商品を後に表示
