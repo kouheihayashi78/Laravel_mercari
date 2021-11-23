2b5c69cfba82613623e9f6f19b6f47b2a637487a
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-11-23 15:47:14 +0900

    修正

--- app/Http/Requests/SellRequest.php
+++ app/Http/Requests/SellRequest.php
@@ -24,7 +24,7 @@ class SellRequest extends FormRequest
     public function rules()
     {
         return [
-            'item-image'  => ['required', 'file', 'image'],
+            'item'  => ['required', 'file', 'image'],
             'name'        => ['required', 'string', 'max:255'],
             'description' => ['required', 'string', 'max:2000'],
             'category'    => ['required', 'integer'],
